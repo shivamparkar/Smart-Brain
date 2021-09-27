7379e7b027232ddd34a742640ff888303295aa41
shivamparkar <parkarshivam@gmail.com>
2021-09-27 19:54:42 +0530

    created FaceRecogintion

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -5,6 +5,7 @@ import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
 import Particles from 'react-particles-js';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 
 const particlesOptions = {
     particles: {
@@ -20,6 +21,32 @@ const particlesOptions = {
     
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict("d81cf0fdb9cc4423b3767789f1157433", 'https://samples.clarifai.con/face-det.jpg')
+    .then(
+    function(response){
+      console.log(response);
+    },
+    function(err){
+      
+    }
+    );
+}
+
+
   render(){
   return (
     <div className="App">
@@ -29,8 +56,9 @@ class App extends Component {
      <Navigation />
      <Logo />
      <Rank />
-     <ImageLinkForm />
-     {/* <FaceRecognition />  */}
+     <ImageLinkForm onInputChange={this.onInputChange} onButoonSubmit={this.onButtonSubmit}
+      />
+     <FaceRecognition />  
     </div>
    );
  }

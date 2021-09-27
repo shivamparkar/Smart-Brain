5de6d198c9f0e261844ea17e4cc5d21723feca37
shivamparkar <parkarshivam@gmail.com>
2021-09-27 15:07:11 +0530

    Adding all files

---
+++ smart-brain/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+    particles: {
+      number: {
+        value: 30,
+        denisty: {
+          enable: true,
+          value_area: 800
+        }
+      }
+    }
+  }
+    
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+     <Particles className='particles'
+              params={particlesOptions}
+              />
+     <Navigation />
+     <Logo />
+     <Rank />
+     <ImageLinkForm />
+     {/* <FaceRecognition />  */}
+    </div>
+   );
+ }
+}
+
+export default App;
